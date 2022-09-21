49bb268be2e9ca9883fbb1db86f9b3d6d129491d
lidiettes <hello@lidiettes.com>
2022-09-21 09:35:06 +0200

    cambios controller list

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -6,8 +6,19 @@ require ('employeeManager.php');
 
 // getUsers();
 
-printEmployees();
 
+
+
+if(isset($_GET["action"])&& $_GET["action"]=="list"){
+    echo printEmployees();
+    };
+    
+    if(isset($_GET["action"])&&$_GET["action"]=="addEmployee"){
+    
+        $employees = file_get_contents('../resources/employees.json');
+    
+        echo $employees;
+        };
 
 
 
