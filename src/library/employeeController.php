c508131e3c446836a7d7b5d579bb7bc07de1e10b
Lokesh Pereiro <lpdraft001@gmail.com>
2022-09-23 18:01:34 +0200

    addEmploy Function done

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -5,20 +5,22 @@ require ('employeeManager.php');
 // ------------------------------------------------------------------------------------------------ //
 if(isset($_GET["action"]) && $_GET["action"] === "listEmployees"){
     echo getEmployeesData();
-    };
     
-
-
-
-if(isset($_GET['action']) && $_GET['action'] === 'list' ){
-    printEmployees();
+    }else if (isset($_GET['action']) && $_GET['action'] === 'createEmploy'){
+        $newEmployee = [
+            "name"          => $_POST['name'],
+            "lastName"      => $_POST['lastName'],
+            "email"         => $_POST['email'],
+            "gender"        => $_POST['gender'],
+            "city"          => $_POST['city'],
+            "streetAddress" => $_POST['streetAddress'],
+            "state"         => $_POST['state'],
+            "age"           => $_POST['age'],
+            "postalCode"    => $_POST['postalCode'],
+            "phoneNumber"   => $_POST['phoneNumber'],
+        ];
+        addEmployee($newEmployee);
+    }
     
-    
-} else if  (isset($_GET['action']) && $_GET['action'] === 'delete' ){
-    $employeeId = $_GET['id'];
-    deleteEmployee($employeeId);
-
-}   
-
 
 ?>
