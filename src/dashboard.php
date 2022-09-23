e4e3ed81b746a9d7a77d69ebf5e71d62b138caa8
Lokesh Pereiro <lpdraft001@gmail.com>
2022-09-23 11:27:58 +0200

    have my old functions

--- src/dashboard.php
+++ src/dashboard.php
@@ -48,104 +48,90 @@ require ('./library/employeeController.php');
 </head>
 <body>
     <div class="container">
-        <h1 class="title">Welcome Lovelance Contents</h1>
-        <a href="../index.php" class="btn btn-sm btn btn-warning">Log Out</a>
-        <!-- <a href="#" id="createEmploy" class="btn btn-sm btn btn-success">Create</a> -->
+        <h1 class="title">Dashboard</h1>
+        <a href="../index.php" class="btn btn-sm btn btn-danger p-1"><i class="bi bi-box-arrow-left p-1"></i>Log Out</a>
 
+        <hr class="border">
+        <div class="content">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-sm btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-person-plus"></i> New Employee</button>
 
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
-          Create Modal Employees
-        </button>
-
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-             
-                <h5 class="modal-title" id="exampleModalLabel">New Employ</h5>
-      
-            <form  id="formData" action="newEmploy">
-              <div class="modal-body">
-                        <div class="form-control">
-                            <label for="name">Name:</label>
-                            <input type="text" name="name">
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">New employee data</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-
-                        <div class="form-control">
-                        <label for="email">Email:</label>
-                        <input type="email" name="email">
+                    <form method="post" id="addNewEmployeeForm">    
+                        <div class="modal-body">
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping">Name</span>
+                                <input type="text" id="name" name="name" class="form-control" placeholder="Enter your name" aria-label="Username" aria-describedby="addon-wrapping">
+                            </div>
+                            <br>
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping">Last name</span>
+                                <input type="text" id="lastName" name="lastName" class="form-control" placeholder="Enter your last name" aria-label="Username" aria-describedby="addon-wrapping">
+                            </div>
+                            <br>
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping">@</span>
+                                <input type="text" id="email" name="email" class="form-control" placeholder="Enter your email" aria-label="Username" aria-describedby="addon-wrapping">
+                            </div>
+                            <br>
+                            <div class="input-group mb-3">
+                                <label class="input-group-text" for="inputGroupSelect01">Gender</label>
+                                <select class="form-select" id="gender" name="gender" id="inputGroupSelect01">
+                                    <option selected>What gender do you identify with?</option>
+                                    <option value="Man">Man</option>
+                                    <option value="Woman">Woman</option>
+                                    <option value="Another">Another</option>
+                                </select>
+                            </div>
+                            <br>
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping">City</span>
+                                <input type="text" id="city" name="city" class="form-control" placeholder="What city are you from?" aria-label="Username" aria-describedby="addon-wrapping">
+                            </div>
+                            <br>
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping">Street Address</span>
+                                <input type="text" id="streetAddress " name="streetAddress" class="form-control" placeholder="Enter your email" aria-label="Username" aria-describedby="addon-wrapping">
+                            </div>
+                            <br>
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping">State</span>
+                                <input type="text" name="state" class="form-control" placeholder="Enter your state" aria-label="Username" aria-describedby="addon-wrapping">
+                            </div>
+                            <br>
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping">Age</span>
+                                <input type="text" name="age"  class="form-control" placeholder="How old are you?" aria-label="Username" aria-describedby="addon-wrapping">
+                            </div>
+                            <br>
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping">Postal code</span>
+                                <input type="text" name="postalCode" class="form-control" placeholder="Enter your postal code" aria-label="Username" aria-describedby="addon-wrapping">
+                            </div>
+                            <br>
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping">Phone number</span>
+                                <input type="text" name="phoneNumber" class="form-control" placeholder="Enter your phone number" aria-label="Username" aria-describedby="addon-wrapping">
+                            </div>
                         </div>
-
-                        <div class="form-control">
-                        <label for="age">Age:</label>
-                        <input type="text" name="age">
-                        </div>
-
-                        <div class="form-control">
-                        <label for="street">Street:</label>
-                        <input type="text" name="street">
-                        </div>
-
-                        <div class="form-control">
-                        <label for="state">State:</label>
-                        <input type="text" name="state">
-                        </div>
-
-                        <div class="form-control">
-                        <label for="postalCode">Postal Code:</label>
-                        <input type="text" name="postalCode">
-                        </div>
-
-                        <div class="form-control">
-                        <label for="phoneNumber">Phone Number:</label>
-                        <input type="text" name="phoneNumber">
-                        </div>
-
                         <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <!-- ¿¿¿Name=Submit???? -->
-                        <button type="submit" class="btn btn-primary" name="submit">Add Employ</button>     
+                            <button type="button" class="btn btn-danger p-2" data-bs-dismiss="modal"><i class="bi bi-x-circle p-1"></i>Close</button>
+                            <button type="submit" name="submit" class="btn btn-primary p-2"><i class="bi bi-box-arrow-down p-1"></i>Save employee</button>
                         </div>
-
+                    </form>    
                     </div>
-                    
-        </form>
-            
-
+                </div>
             </div>
-          </div>
-        </div>
 
-        <hr class="border">
-            <div class="content">
-
-                <table class="table mt-5">
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Age</th>
-                        <th>Street</th>
-                        <th>City</th>
-                        <th>State</th>
-                        <th>Postal Code</th>
-                        <th>Phone number</th>
-                        <th>Open</th>
-                        <th>Delete</th>
-                    </tr>
-                    </thead>
-                    <tbody id="tbody">
-                        <!-- AddEventlister a tbody para insertar [tr] -->
-
-                    </tbody>
-        <a href="../index.php" class="btn btn-sm btn btn-danger">Log Out</a>
-        <a href="" class="btn btn-sm btn btn-success">Create</a>
-        <hr class="border">
-            <div class="content">
-
-                <table class="table mt-5">
-                    <thead>
+            <table class="table mt-5">
+                <thead>
                     <tr>
                         <th>Name</th>
                         <th>Email</th>
@@ -155,16 +141,14 @@ require ('./library/employeeController.php');
                         <th>State</th>
                         <th>Postal Code</th>
                         <th>Phone number</th>
+                        <th>Open</th>
                         <th>Delete</th>
-                        <th>Update</th>
                     </tr>
-                    </thead>
-            
-        
-                    <tbody id="tbody"></tbody>
-                </table>
+                </thead>
+            <tbody id="tbody"></tbody>
+            </table>
 
-            </div>
+        </div>
     </div>
 </body>
 </html>
